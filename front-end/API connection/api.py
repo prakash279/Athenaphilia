b201892a53768d18562bbe6fbd423a8e2bfaab14
Prakashchand Choudhary <89769921+prakash279@users.noreply.github.com>
2024-02-27 21:50:07 +0530

    Update api.py

--- front-end/API connection/api.py
+++ front-end/API connection/api.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
+# from fastapi import FastAPI
 
-app = FastAPI()
+# app = FastAPI()
 
-@app.get("/")
-def read_root():
-  return{"Hello":"World"}
+# @app.get("/")
+# def read_root():
+#   return{"Hello":"World"}
+
+# This is a basic code snippet of fast API and it will be used to send requests to the LLM models receive the output and print it on the website according to the needs
